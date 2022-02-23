bc8eca92a7f3dc3a64cbe5ed98a2f2c36f29637c
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-23 18:49:27 -0300

    feat: vertex buffers and "drawing" a triangle

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 int main(void)
 {
     GLFWwindow* window;
@@ -22,11 +24,35 @@ int main(void)
 
     gladLoadGL();
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] =
+    {
+        // Cartesian plan coordinates to our triangle
+        -0.5f, -0.5f, // bottom left corner
+         0.0f,  0.5f, // middle top corner
+         0.5f, -0.5f  // bottom right corner
+    };
+
+    unsigned int buffer;
+
+    // Creates a buffer and stores it in the before declarated variable "buffer".
+    glGenBuffers(1, &buffer);
+
+    // Bind is basically: select this specific buffer, buffer beign a previously generated buffer.
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    // Here we put the actual data to a triangle in our previously created buffer
+    // LINK: https://docs.gl/gl4/glBufferData
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
